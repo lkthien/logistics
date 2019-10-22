62a317dd82140169dc30dc41d64bbdef6d89521f
lkthien <lkthien@tma.com.vn>
2019-10-22 18:27:19 +0700

    add client

---
+++ client/src/routers/content.router.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from "../pages/home.page";
+import InfoPage from "../pages/info.page";
+
+const ContentRouter = () => {
+    return (
+        <React.Fragment>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route path="/info" component={InfoPage} />
+            </Switch>
+        </React.Fragment>
+    )
+}
+export default ContentRouter;
