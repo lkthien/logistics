ab75a8363c74f292a1068bd7c0b2f34e3b763f54
lkthien <lkthien@tma.com.vn>
2019-10-23 17:53:31 +0700

    Update planet.component.js

--- client/src/components/planet.component.js
+++ client/src/components/planet.component.js
@@ -1,36 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React,  { useState, Component } from "react";
 
-const Planets = () => {
-    const [hasError, setErrors] = useState(false);
-    const [planets, setPlanets] = useState({});
-
-    async function fetchData() {
-        let headers = new Headers();
-
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        headers.append('Origin', 'http://localhost:3001/api/accounts');
-        const res = await fetch({
-            mode: 'cors',
-            method: 'GET',
-            headers: headers
-        });
-        res
-            .json()
-            .then(res => setPlanets(res))
-            .catch(err => setErrors(err));
+export default class Planets extends Component {
+    state = {
+      hasErrors: false,
+      planets: {}
+    };
+    
+    componentDidMount() {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Accept', 'application/json');
+    headers.append('Origin', 'http://localhost:3001/api/accounts');
+      fetch({
+        mode: 'cors',
+        method: 'GET',
+        headers: headers
+    })
+        .then(res => res.json())
+        .then(res => {this.setState({ planets: res}); console.log(res)})
+        .catch(() => this.setState({ hasErrors: true }));
     }
-
-    useEffect(() => {
-        fetchData();
-    });
-
-    return (
-        <div>
-            <span>{JSON.stringify(planets)}</span>
-            <hr />
-            <span>Has error: {JSON.stringify(hasError)}</span>
-        </div>
-    );
-};
-export default Planets;
+  
+    render() {
+      return <div>{JSON.stringify(this.state.planets)}</div>;
+    }
+  }
